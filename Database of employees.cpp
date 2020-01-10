62490dc7f8640726964002e61368e960a7950dc7
AmaaniGoose <55527135+AmaaniGoose@users.noreply.github.com>
2020-01-10 12:27:22 +0530

    Add files via upload

---
+++ Database of employees.cpp
@@ -0,0 +1,118 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Employee{
+	int Emp_id;
+	char Emp_name[100];
+	char Emp_city[100];
+};
+void swap(struct Employee *a,struct Employee  *b)
+{
+	struct Employee temp=*a;
+	*a=*b;
+	*b=temp;
+}
+struct Employee *CreateDatabase(void)
+{int i;
+	struct Employee *ptr=(struct Employee*)malloc(2*sizeof(struct Employee));
+	for(i=0;i<sizeof(ptr)/sizeof(ptr[0]);i++)
+	{
+		scanf("%d",&ptr[i].Emp_id);
+		gets(ptr[i].Emp_name);
+		gets(ptr[i].Emp_city);
+	}
+	return ptr;
+}
+int search(struct Employee *ptr,int Emp_id)
+{int i;
+	for(i=0;i<sizeof(ptr)/sizeof(ptr[0]);i++)
+	{
+		if(Emp_id==ptr[i].Emp_id)
+		{return i;
+		 break;
+		}
+	}
+}
+void sort(struct Employee *ptr)
+{int i;
+	for(i=0;i<sizeof(ptr)/sizeof(ptr[0]);i++)
+		{	
+			if(ptr[i].Emp_id>ptr[i+1].Emp_id)
+				{
+					swap(ptr[i],ptr[i+1]);
+				}
+		}
+	
+}
+int countemp(struct Employee *ptr)
+{
+	return sizeof(ptr)/sizeof(ptr[0]);
+}
+struct Employee *addrec(struct Employee *ptr)
+{int org=sizeof(ptr)/sizeof(ptr[0]),i;
+	ptr=realloc(ptr,(2+sizeof(ptr))*sizeof(struct Employee));
+	for(i=org;i<sizeof(ptr)/sizeof(ptr[0]);i++)
+	{
+		scanf("%d",&ptr[i].Emp_id);
+		gets(ptr[i].Emp_name);
+		gets(ptr[i].Emp_city);
+	}
+	return ptr;
+}
+void printitem(struct Employee *ptr,int index)
+{int i=index;
+	printf("%d\n",ptr[i].Emp_id);
+	printf("%s\n",ptr[i].Emp_name);
+	printf("%s\n",ptr[i].Emp_city);
+	
+}
+void printall(struct Employee *ptr)
+{int i;
+for(i=0;i<sizeof(ptr)/sizeof(ptr[0]);i++)
+{
+
+	printf("%d\n",ptr[i].Emp_id);
+	printf("%s\n",ptr[i].Emp_name);
+	printf("%s\n",ptr[i].Emp_city);
+}
+}
+
+int main()
+{
+
+struct Employee *A=CreateDatabase();
+int ch,ch2=1;
+while(ch2==1)
+{
+
+printf("Enter 1 for Searching EMP data using Empid, 2 for Sorting, 3 for counting, 4 for increasing size: ");	
+scanf("%d",&ch);
+switch(ch)
+{
+	case 1: int id;
+			scanf("%d",&id);
+			int index=search(A,id);
+			printitem(A,index);
+			
+	break;
+	
+	case 2: sort(A);
+			printall(A);
+	break;
+	
+	case 3: int count=countemp(A);
+			printf("%d",count);
+	
+	break;
+	
+	case 4: addrec(A);
+			printf("Size successfully increased");
+	
+	break;
+	 
+}
+printf("Enter more operations? 1 for yes 0 for no");
+scanf("%d",&ch2);
+}
+	
+	
+}
--- Occurences of Element.cpp
+++ Occurences of Element.cpp
@@ -1,21 +1,43 @@
 #include<stdio.h>
-int main()
-{
-int N,i;
-scanf("%d",&N);
-int A[N];
-for(i=0;i<N;i++)
+#include<stdbool.h>
+void countFreq(int arr[], int n) 
+{ 
+int i;
+bool visited[n];
+for(i=0;i<n;i++)
+{	
+	visited[i]=false;
+}
+ 
+    for (int i = 0; i < n; i++) { 
+  
+        if (visited[i] == true) 
+            continue; 
+  
+        int count = 1; 
+        for (int j = i + 1; j < n; j++) 
+		{ 
+            if (arr[i] == arr[j]) { 
+                visited[j] = true; 
+                count++; 
+            } 
+        } 
+        printf("%d -> %d\n",arr[i],count);
+    
+    } 
+} 
+  
+int main() 
+{ 
+   int n;
+scanf("%d\n",&n);
+int A[n];
+for(int i=0;i<n;i++)
+{A[i]=0;
+}
+for(int i=0;i<n;i++)
 {scanf("%d",&A[i]);
 }
-int ele,count=0;
-scanf("%d",&ele);
-for(i=0;i<N;i++)
-{
-if(A[i]==ele)
-{
-count++;
-}
-}
-printf("Number of Occurences of %d are : %d",ele,count);
-return 0;
-}
+    countFreq(A, n); 
+    return 0; 
+} 
