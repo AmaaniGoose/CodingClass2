dd6620e83550d11220be0d3fc761753a9cc01982
AmaaniGoose <55527135+AmaaniGoose@users.noreply.github.com>
2020-01-31 10:32:33 +0530

    Add files via upload

---
+++ Circularly Q2.cpp
@@ -0,0 +1,76 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+	 { 
+		 int data; 
+		 struct node *link;	  	
+	 };
+struct node *head;
+
+node* getnode(int data) 
+{ 
+
+    node* newNode = (node*)malloc(sizeof(node)); 
+  	newNode->data = data; 
+    newNode->link = NULL; 
+    return newNode; 
+} 
+
+void list(int n)
+{
+struct node *temp,*newnode;
+int i,data,count=0;
+head=(struct node*)malloc(sizeof(node));
+if(head==NULL)
+{
+	printf("Insufficient space");
+}
+printf("Enter data of first node: ");
+scanf("%d",&data);
+head->data=data;
+head->link=NULL;
+temp=head;
+count++;
+for(i=0;i<n-1;i++)
+{
+	printf("Enter data for node %d: ",count+1);
+	scanf("%d",&data);
+	newnode=getnode(data);
+	temp->link=newnode;
+	newnode->link=head;
+	temp=newnode;
+	count++;
+}
+}
+void printall()
+{
+	struct node* temp;
+	temp=head;
+	while(temp->link!=head)
+	{
+		printf("%d->",temp->data);
+		temp=temp->link;
+	}
+	printf("%d->",temp->data);
+}
+void count()
+{
+	struct node *temp;
+	temp=head;
+	int count=1;
+	while(temp->link!=head)
+	{
+		temp=temp->link;
+		count++;
+	}
+	printf("\n%d",count);
+}
+int main()
+{
+	int n;
+	printf("Enter the value of n: ");
+	scanf("%d",&n);
+	list(n);
+	printall();
+	count();
+}
